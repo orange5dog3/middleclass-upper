5ea15353be83a0f10221b29d503f67db1b89aa75
orange5dog3 <orange5dog3@gmail.com>
2025-06-22 20:20:00 +0900

    hamburger

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
 // hamburger------------------------
-$(document).ready(function() {
-    $(".hamburger-trigger").click(function() {
-        $(".hamburger").toggleClass("active");
-        $(".hamburger-menu").toggleClass("show");
-        $("#hamburger-bg").toggleClass("active");
-    });
-    
-    // 背景クリックでメニューを閉じる
-    $("#hamburger-bg").click(function() {
-        $(".hamburger").removeClass("active");
-        $(".hamburger-menu").removeClass("show");
-        $("#hamburger-bg").removeClass("active");
-    });
+$(".hamburger-trigger").click(
+    function() {
+    $(".hamburger-menu").toggleClass("active")
+    $(".hamburger").toggleClass("active")
+    $("#hamburger-bg").toggleClass("active")
 });
