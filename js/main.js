6cdc28528b9a896225c4a27f3b69d0bade9b0e11
orange5dog3 <orange5dog3@gmail.com>
2025-07-04 20:26:12 +0900

    アニメーション完了

--- js/main.js
+++ js/main.js
@@ -47,6 +47,23 @@ $(window).scroll(function() {
     
     if (scrollTop > threshold) {
         $('#upper-arrow').fadeIn();
+        
+        // footer制御ロジック
+        const scrollHeight = $(document).height();/*ページ全体の高さ*/
+        const scrollPosition = $(window).height() + $(window).scrollTop();/*ページの一番上からスクロールされた距離*/
+        const footHeight = $("footer").outerHeight();/*フッターの高さ*/
+
+        if ( scrollHeight - scrollPosition  <= footHeight ) {
+            $("#upper-arrow").css({
+                "position":"absolute",
+                "bottom": footHeight + 90, // footer上6rem分の位置
+            });
+        } else {
+            $("#upper-arrow").css({
+                "position":"fixed",
+                "bottom": "9rem",
+            });
+        }
     } else {
         $('#upper-arrow').fadeOut();
     }
@@ -59,6 +76,40 @@ $('#upper-arrow a').click(function() {
     }, 500);
     return false;
 });
+
+// ==========================================================================
+// contact button footer control
+// ==========================================================================
+$(function() {
+
+    $(window).on("scroll", function() {
+        if ($(this).scrollTop() > 100) {
+            $("#contact-btn").fadeIn(300);
+            } else {
+            $("#contact-btn").fadeOut(300);
+        }
+
+        const scrollHeight = $(document).height();/*ページ全体の高さ*/
+        const scrollPosition = $(window).height() + $(window).scrollTop();/*ページの一番上からスクロールされた距離*/
+        const footHeight = $("footer").outerHeight();/*フッターの高さ*/
+
+        if ( scrollHeight - scrollPosition  <= footHeight ) {
+            $("#contact-btn").css({
+                "position":"absolute",
+                "bottom": footHeight,
+            });
+        } else {
+            $("#contact-btn").css({
+                "position":"fixed",
+                "bottom": "0",
+            });
+        }
+    });
+
+});
+
+
+
 
 // ==========================================================================
 // voice slider
